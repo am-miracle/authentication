27cb2dd2eb34a46cd79ea63aa3a50fecf3913dd3
Miracle Jude <judemiraco0@gmail.com>
2023-02-25 13:32:02 +0100

    initial level1 commit

---
+++ app.js
@@ -0,0 +1,73 @@
+//jshint esversion:6
+const express = require('express');
+const bodyParser = require("body-parser");
+const ejs = require('ejs');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose")
+
+dotenv.config({ path: '.env' });
+
+const app =  express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.set("view engine", 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect(process.env.CONNECTION_URL)
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User = mongoose.model("user", userSchema);
+
+
+app.get("/", (req, res) => {
+    res.render("home")
+})
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+app.get("/register", (req, res) => {
+    res.render("register")
+})
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save((err) => {
+        if(err) {
+            console.log(err)
+        }
+        else {
+            res.render('secrets')
+        }
+    })
+})
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, (err, foundUser) => {
+        if(err){
+            console.log(err)
+        }
+        else {
+            if(foundUser) {
+                if(foundUser.password === password) {
+                    res.render('secrets')
+                }
+            }
+        }
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
